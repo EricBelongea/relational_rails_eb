33af3d41c8de607f706b58525f1f28e101fed341
Eric Belongea <eric.belongea@gmail.com>
2023-10-12 17:18:08 -0600

    Adds user story six

--- spec/features/fields/field_spec.rb
+++ spec/features/fields/field_spec.rb
@@ -34,5 +34,27 @@ RSpec.describe "#Fields" do
         expect(page).to have_content("Total Sends: #{new_joes.total_ticks}")
       end
     end
+
+    describe "When I visit the parent index" do
+      it "I see that records are ordered by the most recently created first" do
+        new_joes = Field.create(field_name: "New Joes", in_season: true, total_ticks: 101)
+        resident_evil = new_joes.boulders.create(name: "Resident Evil", v_grade: 9, tick: true)
+        chips = new_joes.boulders.create(name: "Chips", v_grade: 6, tick: true)
+        planet = new_joes.boulders.create(name: "Planet of the Apes", v_grade: 7, tick: true)
+        ghost_king = new_joes.boulders.create(name: "Ghost King", v_grade: 11, tick: false)
+
+        left_fork = Field.create(field_name: "Left Fork", in_season: true, total_ticks: 666)
+        wills_afire = left_fork.boulders.create(name: "Wills Afire", v_grade: 6, tick: true)
+        angler = left_fork.boulders.create(name: "The Angler", v_grade: 2, tick: true)
+        kill_by_numbers = left_fork.boulders.create(name: "Kill by Numbers", v_grade: 5, tick: false)
+        beyond_life = left_fork.boulders.create(name: "Beyond Life", v_grade: 10, tick: true)
+
+        visit "/fields"
+
+        expect(new_joes.field_name).to appear_before(left_fork.field_name)
+        expect(page).to have_content(new_joes.created_at)
+        expect(page).to have_content(left_fork.created_at)
+      end
+    end
   end
 end
