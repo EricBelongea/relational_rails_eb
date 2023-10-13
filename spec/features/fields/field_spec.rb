5ed22851aa6db70c8c115aae42532a8b2a18aee0
Eric Belongea <eric.belongea@gmail.com>
2023-10-12 19:29:49 -0600

    Adds user story eight and nine

--- spec/features/fields/field_spec.rb
+++ spec/features/fields/field_spec.rb
@@ -71,6 +71,7 @@ RSpec.describe "#Fields" do
         kill_by_numbers = left_fork.boulders.create(name: "Kill by Numbers", v_grade: 5, tick: false)
         beyond_life = left_fork.boulders.create(name: "Beyond Life", v_grade: 10, tick: true)
         jordan = left_fork.boulders.create(name: "They Call Him Jordan", v_grade: 10, tick: false)
+
         visit "/fields/#{new_joes.id}"
 
         expect(page).to have_content("Total Boulders: 4")
@@ -78,6 +79,28 @@ RSpec.describe "#Fields" do
         visit "fields/#{left_fork.id}"
 
         expect(page).to have_content("Total Boulders: 5")
+      end
+    end
+
+    describe 'When I visit any page on the site' do
+      it 'I see a link on top of the page that takes me to /fields' do
+        new_joes = Field.create(field_name: "New Joes", in_season: true, total_ticks: 101)
+        resident_evil = new_joes.boulders.create(name: "Resident Evil", v_grade: 9, tick: true)
+        chips = new_joes.boulders.create(name: "Chips", v_grade: 6, tick: true)
+        planet = new_joes.boulders.create(name: "Planet of the Apes", v_grade: 7, tick: true)
+        ghost_king = new_joes.boulders.create(name: "Ghost King", v_grade: 11, tick: false)
+
+        left_fork = Field.create(field_name: "Left Fork", in_season: true, total_ticks: 666)
+        wills_afire = left_fork.boulders.create(name: "Wills Afire", v_grade: 6, tick: true)
+        angler = left_fork.boulders.create(name: "The Angler", v_grade: 2, tick: true)
+        kill_by_numbers = left_fork.boulders.create(name: "Kill by Numbers", v_grade: 5, tick: false)
+        beyond_life = left_fork.boulders.create(name: "Beyond Life", v_grade: 10, tick: true)
+        jordan = left_fork.boulders.create(name: "They Call Him Jordan", v_grade: 10, tick: false)
+
+        visit "/fields/#{new_joes.id}"
+
+        expect("Home").to appear_before("All Boulders")
+        expect("All Boulders").to appear_before("New Joes")
       end
     end
   end
